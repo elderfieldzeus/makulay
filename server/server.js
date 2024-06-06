cb957d43f94c7ca30628d7534a72b8170802fb17
elderfieldzeus <elderfieldzeus24@gmail.com>
2024-06-06 23:43:30 +0800

    added first few initialization files

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.static(__dirname));
+
+app.use("/", require("./routes"));
+
+app.listen(PORT, console.log(`Listening on port ${PORT}`));
